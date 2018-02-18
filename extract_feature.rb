2c33b792d765c14817d3ac0b8cf8e76959a37bd5
vitro <vitro@hackisfun.local>
2018-02-18 18:20:32 +0900

    正解データの抽出ルーチン作成した

--- extract_feature.rb
+++ extract_feature.rb
@@ -2,6 +2,7 @@ require 'json'
 
 docs = []
 freqs = {}
+categories = {}
 while (line = $stdin.gets)&.chomp!
   n = line.split("$")
   ords = n[0].chars.map(&:ord)
@@ -9,13 +10,18 @@ while (line = $stdin.gets)&.chomp!
     freqs[ord] ||= 0
     freqs[ord] += 1
   }
+  len = categories.length
+  categories[n[1]] = {
+    count: (categories[n[1]]&.[](:count) || 0) + 1,
+    len: (categories[n[1]]&.[](:len) || len)
+  }
   docs << {
     t: n[1],
+    t_id: categories[n[1]][:len],
     v: n[0],
-    feature: ords.reduce({}) {|memo, c|
+    feature: ords.each_with_object({}) {|c, memo|
       memo[c] ||= 0
       memo[c] += 1
-      memo
     }
   }
 end
@@ -25,7 +31,7 @@ idf = freqs.map {|ord, count|
   [ord, Math.log(D / count.to_f)]
 }.to_h
 
-res = docs.map do |d|
+items = docs.map do |d|
   d[:feature] =
     d[:feature].map {|ord, feature|
     [ord, idf[ord] * feature.to_f]
@@ -33,5 +39,13 @@ res = docs.map do |d|
   d
 end
 
-puts res.to_json
+answers = categories.each_with_object({}) {|(key, value), memo|
+  memo[value[:len]] = {
+    count: value[:count],
+    name: key,
+    id: value[:len],
+  }
+}
 
+pp answers
+# puts({answers: answers, items: items}.to_json)
