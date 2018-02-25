7a38d3976d59ae655b78b79d7a0df9c8a9581894
vitro <vitro@hackisfun.local>
2018-02-25 17:55:50 +0900

    勾配は正しく計算できるようにした

--- extract_feature_sample.sh
+++ extract_feature_sample.sh
@@ -3,8 +3,8 @@ cat data/sample.txt | ruby -rjson -lne "`cat <<EOF
 d = \\$_.split(" ")
 puts ({
   class_name: d[0],
-  class_id: d[1].ord - "A".ord,
+  class_id: d[0].ord - "A".ord,
   feature_name: d[1..-1].join(" "),
-  feature_vectors: [[*0..(d.length - 2)], d[1..-1].map(&:to_i)].transpose.to_h,
+  feature_vector: [[*0..(d.length - 2)], d[1..-1].map(&:to_i)].transpose.to_h,
 }.to_json)
-EOF`" | jq -s '{items: [.[]]}'
+EOF`" | jq -s '{class_size: 3, items: [.[]]}'
--- learn_weight.rb
+++ learn_weight.rb
@@ -1,19 +1,35 @@
 # * scheme *
+# class_size: 3
 # items:
 #   - 
 #     class_name: "A"
 #     class_id: 0
 #     feature_name: "0 1 0"
-#     feature_vectors:
+#     feature_vector:
 #       A: 2
 #       B: 5
+require 'json'
 
-$learning_feature_vectors = [] # => N x D
-$learning_answers = [] # => N x K
+$feature_vectors = nil # => N x D
+$answers = nil # => N x K
 # weights => K X D
 
+def read_vectors(filename)
+  data = JSON.parse(File.read(filename))
+  items = data['items']
+  csize = data['class_size']
+  feature_vectors = items.map {|x| x['feature_vector']}
+  answers = items.map {|item| Array.new(csize, 0.0)}
+  weight_feature = feature_vectors.map(&:values).flatten.uniq.map {|x| [x, 0.0]}.to_h
+  weights = Array.new(csize, weight_feature.dup)
+  [feature_vectors, answers, weights]
+end
+
+$feature_vectors, $answers, $weights =
+                            read_vectors("data/sample.json")
+
 def in_product(weight, feature_vector)
-  (weight.keys + feature_vecotr.keys)
+  (weight.keys + feature_vector.keys)
     .group_by {|k| k}
     .select {|k, v| v.count > 1}
     .map(&:first)
@@ -30,14 +46,24 @@ def probs(weights, feature_vector)
   probs.map {|x| x / sum}
 end
 
-def diff_slope(weights, k, d)
-  memo = []
-  $learn_feature_vectors.each.with_index do |feature_vector, n|
+def with_diff_slope(weights, &block)
+  $feature_vectors.each.with_index do |feature_vector, n|
     probs(weights, feature_vector).each.with_index do |prob, k|
-      memo[k] ||= []
-      memo[k][d] += (prob - $learning_answers[n][k]) * feature_vector[d]
+      block.(feature_vector, n, prob, k)
+    end
+  end
+end
+
+def slope_diff(weights)
+  memo = []
+  with_diff_slope(weights) do |feature_vector, n, prob, k|
+    memo[k] ||= {}
+    feature_vector.each do |d, elem|
+      memo[k][d] ||= 0.0
+      memo[k][d] += (prob - $answers[n][k]) * elem
     end
   end
   memo
 end
 
+p slope_diff($weights)
