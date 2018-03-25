d802b833a14fc38913039623ee992b5b32721824
vitro <vitro@hackisfun.local>
2018-03-25 20:33:53 +0900

    [BUILD] test.9

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
-echo $(pwd)
 $(aws ecr --profile system-docker get-login --no-include-email --region ap-northeast-1)
 URL=429084355607.dkr.ecr.ap-northeast-1.amazonaws.com/kaomoji/classifier
 tag=$(git rev-parse --short HEAD)
