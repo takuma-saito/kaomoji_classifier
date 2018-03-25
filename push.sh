d3c5fa795303a11c80185378561c796c3bca9402
vitro <vitro@hackisfun.local>
2018-03-25 20:34:48 +0900

    [BUILD] test.11

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 $(aws ecr --profile system-docker get-login --no-include-email --region ap-northeast-1)
 URL=429084355607.dkr.ecr.ap-northeast-1.amazonaws.com/kaomoji/classifier
-tag=$(git rev-parse --short HEAD)
+tag=$(git --git-dir=.git rev-parse --short HEAD)
 docker build -t classifier:$tag -f Dockerfile .
 docker tag classifier:$tag $URL:$tag
 docker tag classifier:$tag $URL:latest
