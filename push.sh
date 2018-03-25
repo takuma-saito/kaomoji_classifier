764faf3481a55676377d1423dd4c70da7fc3ae2c
vitro <vitro@hackisfun.local>
2018-03-25 20:24:41 +0900

    push.sh を AWS 向けにした

--- push.sh
+++ push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -xe
-docker build -t classifier:$(git rev-parse --short HEAD) -f Dockerfile .
-docker tag classifier:$(git rev-parse --short HEAD) localhost:5000/classifier:$(git rev-parse --short HEAD)
-docker tag classifier:$(git rev-parse --short HEAD) localhost:5000/classifier:latest
-docker push localhost:5000/classifier:latest
-docker push localhost:5000/classifier:$(git rev-parse --short HEAD)
-git push origin $(git rev-parse --short HEAD):build/$(git rev-parse --short HEAD)
+$(aws ecr --profile system-docker get-login --no-include-email --region ap-northeast-1)
+URL=429084355607.dkr.ecr.ap-northeast-1.amazonaws.com/kaomoji/classifier
+tag=$(git rev-parse --short HEAD)
+docker build -t classifier:$tag -f Dockerfile .
+docker tag classifier:$tag $URL:$tag
+docker tag classifier:$tag $URL:latest
+docker push $URL:latest
+docker push $URL:$tag
+git tag build/$tag
+git push origin build/$tag:build/$tag
