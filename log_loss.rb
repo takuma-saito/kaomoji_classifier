90a2f4767a4291d5d5e71ac1ce15fcdb5a1919e8
vitro <vitro@hackisfun.local>
2018-05-05 12:19:48 +0900

    log_loss リファクタリング

--- log_loss.rb
+++ log_loss.rb
@@ -2,10 +2,14 @@ require_relative 'common'
 sum = 0
 n = 0
 dir = ARGV[0]
-with_predictions(dir, '$', File.open(dir + '/cross_validation_data.txt')) do |text, predictions, answer_category|
+filename = 'cross_validation_data.txt'
+# filename = 'training_data.txt'
+with_predictions(dir, '$', File.open(dir + '/' + filename)) do |text, predictions, answer_category|
   prediction = predictions.select do |prediction|
     prediction['name'] == answer_category
   end&.first || next
+  pp Math.log(prediction['prob']), text, answer_category, predictions.take(5)
+  puts ''
   sum += -Math.log(prediction['prob'])
   n += 1
 end
