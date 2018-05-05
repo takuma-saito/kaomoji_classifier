f2421ca232e5af349139a1ff4fafca22a3cef1bb
vitro <vitro@hackisfun.local>
2018-05-05 19:17:44 +0900

    スクリプト修正

--- gen_B.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -xe
-cat training_data/data_set_0/cross_validation_data.txt | ruby predicted.rb training_data/data_set_0 | sort -R | head -n 20
--- predicted.rb
+++ predicted.rb
@@ -2,7 +2,7 @@ require_relative 'common'
 
 with_predictions(ARGV[0], '$') do |text, predictions, answer_category|
   ps = predictions.take(3)
-  puts "#{text},#{ps.first['name']},#{answer_category}," +
+  puts "#{text},#{answer_category},#{ps.first['name']}," +
        (ps.first['name'] == answer_category ? 'o,' : 'x,') +
        ps.map {|prediction|
     "#{prediction['name']}: #{sprintf("%.2f", prediction['prob'])}"
