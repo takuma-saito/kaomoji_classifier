524806b881df7896791f4b0def1b2a64d131620d
root <root@ik1-336-28083.vs.sakura.ne.jp>
2018-05-05 16:27:25 +0900

    学習済みデータ追加

--- run-training.sh
+++ run-training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNT=$1
-seq 0 5 | while read num; do
+echo 0 | while read num; do
     ruby learn_weight.rb training_data/data_set_${num}/training_data.json ${COUNT} 2>(tee training_data/data_set_${num}/stderr.log) |
         jq '.' > training_data/data_set_${num}/weights.json
 done
