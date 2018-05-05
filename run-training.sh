b6df7c45e632638b6c3499345caeecd86fe6fd6b
root <root@ik1-336-28083.vs.sakura.ne.jp>
2018-05-05 18:18:43 +0900

    update data-set-0

--- run-training.sh
+++ run-training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNT=$1
-echo 0 | while read num; do
+seq 0 10 | while read num; do
     ruby learn_weight.rb training_data/data_set_${num}/training_data.json ${COUNT} 2>(tee training_data/data_set_${num}/stderr.log) |
         jq '.' > training_data/data_set_${num}/weights.json
 done
