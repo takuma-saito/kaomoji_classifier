c5f70c560c005bd1b45a6e8c9f4c86f5bc5a43ac
vitro <vitro@hackisfun.local>
2018-05-05 12:45:05 +0900

    クロスバリデーションの数を変更した

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
-seq 0 5 |
-    xargs -I{} bash -lc "ruby -e 'puts File.open(\"data/kaomoji.txt\").readlines.rotate(157 * {}).take(942 - 157).join' > training_data/data_set_{}/training_data.txt"
-seq 0 5 |
-    xargs -I{} bash -lc "ruby -e 'puts File.open(\"data/kaomoji.txt\").readlines.rotate(157 * ({} - 1)).take(157).join' > training_data/data_set_{}/cross_validation_data.txt"
-seq 0 5 |
+seq 0 10 |
+    xargs -I{} mkdir -p training_data/data_set_{}
+seq 0 10 |
+    xargs -I{} bash -lc "ruby -e 'puts File.open(\"data/kaomoji.txt\").readlines.rotate(85 * {}).take(942 - 85).join' > training_data/data_set_{}/training_data.txt"
+seq 0 10 |
+    xargs -I{} bash -lc "ruby -e 'puts File.open(\"data/kaomoji.txt\").readlines.rotate(85 * ({} - 1)).take(85).join' > training_data/data_set_{}/cross_validation_data.txt"
+seq 0 10 |
     xargs -I{} bash -lc "cat training_data/data_set_{}/training_data.txt | ruby extract_feature.rb | jq '.' > training_data/data_set_{}/training_data.json"
